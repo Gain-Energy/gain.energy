9a358887388a35891c7fe0a1dc3773c4e04e2199
thanh-tai-tran <thanh@gain.energy>
2026-08-03 01:40:42 -0400

    add news

--- src/data/news.js
+++ src/data/news.js
@@ -1,108 +1,114 @@
-import news1 from '../img/news-1.jpg';
-import news2 from '../img/news-2.jpg';
-import news3 from '../img/news-3.jpg';
-import news4 from '../img/news-4.jpg';
-import news5 from '../img/news-5.png';
-import news6 from '../img/news-6.jpg';
-import news7 from '../img/news-7.jpg';
-import news8 from '../img/news-8.png';
-import news9 from '../img/news-9.png';
-import news10 from '../img/news-10.png';
-import news11 from '../img/news-11.png';
-import news12 from '../img/news-12.png';
-import news13 from '../img/news-13.png';
-import news14 from '../img/news-14.png';
-import news15 from '../img/news-15.png';
-import news16 from '../img/news-16.png';
-import news17 from '../img/news-17.png';
+import news1 from "../img/news-1.jpg";
+import news2 from "../img/news-2.jpg";
+import news3 from "../img/news-3.jpg";
+import news4 from "../img/news-4.jpg";
+import news5 from "../img/news-5.png";
+import news6 from "../img/news-6.jpg";
+import news7 from "../img/news-7.jpg";
+import news8 from "../img/news-8.png";
+import news9 from "../img/news-9.png";
+import news10 from "../img/news-10.png";
+import news11 from "../img/news-11.png";
+import news12 from "../img/news-12.png";
+import news13 from "../img/news-13.png";
+import news14 from "../img/news-14.png";
+import news15 from "../img/news-15.png";
+import news16 from "../img/news-16.png";
+import news17 from "../img/news-17.png";
+import news18 from "../img/news-18.png";
 
 const news = [
-    {
-        imageSrc: news17,
-        date: '14 July 2026',
-        text:  `We're launching Upstrima - the first marketplace of specialized AI agents, tools and workflows built exclusively for oil & gas.    Join us to see a live demo of everything Upstrima has to offer from Intelligent data processing to Human-AI Validation Loop and much more. <br/><br/><p style="word-break: break-all;">Please register here: <a target="_blank" rel="noopener noreferrer" href="https://form.jotform.com/261755735879072" style="color: white;">https://form.jotform.com/261755735879072</a><p>`
-    },
-    {
-        imageSrc: news16,
-        date: '22-24 June 2026',
-        text:  `The <span style="font-weight: 600;">Gain.Energy</span> team exhibited at URTeC 2026, where we connected with geoscientists, engineers, operators, and industry leaders while showcasing AI innovations for the future of unconventional resource development. We showcased <span style="font-weight: 600;">Upstrima</span> during the <span style="font-weight: 600;">New Technology Showcase</span> program, where our CEO, Dr. Vlad Karén Payrazyan, delivered the live pre-launch of <span style="font-weight: 600;">UPSTRIMA - </span><span style="font-weight: 600;">the Marketplace of Specialized Oil and Gas AI Agents and Workflows</span>.`
-    },
-    {
-        imageSrc: news15,
-        date: '17 Mar 2026',
-        text:  `The <span style="font-weight: 600;">Gain.Energy</span> team exhibited at <span style="font-weight: 600;">IADC/SPE International Drilling Conference & Exhibition 2026</span>, connecting with industry leaders and engaging in technical discussions on the future of drilling operations. Our booth showcased how Data-Driven AI Agents automate workflows and enhance engineer capabilities in the energy sector. IADC/SPE International Drilling Conference 2026, with its dynamic exchanges and forward-looking sessions, was a major success for Gain.Energy.`
-    },
-    {
-        imageSrc: news14,
-        date: '4 Feb 2026',
-        text:  `Gain.Energy and Labrador invite you to an exclusive AI Workshop in Perth, Australia, on February 4th. This session will concentrate on the practical application of advanced Artificial Intelligence within the Oil and Gas sector, specifically addressing the accelerated adoption of AI across the industry. Our objective is to democratize AI by equipping engineers and domain experts with specialized, accessible tools to foster enhanced efficiency and innovation. <br/><br/>
-        <p style="word-break: break-all;">Please register here to confirm your attendance: <a target="_blank" rel="noopener noreferrer" href="https://form.jotform.com/253565778909275" style="color: white;">https://form.jotform.com/253565778909275</a><p>`
-    },
-    {
-        imageSrc: news13,
-        date: '23 Dec 2025',
-        text: 'Gain.Energy, in collaboration with ONYX iES, cordially invites you to an exclusive AI Workshop session in Kuala Lumpur. This workshop will provide an in-depth examination of the practical application of advanced Artificial Intelligence within the Oil and Gas sector, with a focus on accelerating AI adoption across the industry. Our core objective is to democratize AI for end-users by empowering engineers and domain experts with specialized and accessible AI tools, enabling greater efficiency and innovation.'
-    },
-    {
-        imageSrc: news12,
-        date: '5 November 2025',
-        text: 'Gain.Energy invites you to a live Lunch & Learn on December 2, 2025, at The ION District, Houston, Texas. The session, “Specialized AI Agents for the Oil and Gas Industry: Practical Engineering, Use Cases, Demos, and Limitations,” will focus on the strategic application of Data-Driven AI Agents. This in-depth, hands-on event is for technical specialists and operational leadership, covering real-world engineering, live demos, and an objective analysis of current constraints.'
-    },
-    {
-        imageSrc: news11,
-        date: '3 November 2025',
-        text: 'Dr. Vlad Karén Payrazyan, our CEO, delivered a keynote in the AI Zone at ADIPEC on November 3, showcasing live demonstrations of how Data-Driven AI Agents automate workflows and enhance engineer capabilities in the energy sector. ADIPEC 2025, with its interactive presentations and technical discussions, was a major success for Gain.Energy.'
-    },
-    {
-        imageSrc: news10,
-        date: '3 November 2025',
-        text: 'From November 3–6, Gain.Energy exhibited at ADIPEC 2025 in Abu Dhabi, showcasing our cutting-edge Data-Driven AI Agents transforming oil & gas operations. CEO Dr. Vlad Karén Payrazyan spoke in the AI Zone on automation, augmented engineering, and performance-driven intelligence. Attendees saw live demonstrations of how AI achieves operational excellence and efficiency in upstream and downstream processes. We thank everyone who connected with us.'
-    },
-    {
-        imageSrc: news9,
-        date: '29 October 2025',
-        text: 'Gain.Energy is joining forces with Bardasz to enable the seamless integration of Upstrima AI Solutions directly with real-time drilling & completions data through WITSML. This partnership allows our AI Agents and Workflows to process, interpret, and act on data as it is generated! Soon to come - Live AI Agents.'
-    },
-    {
-        imageSrc: news8,
-        date: '7 August 2025',
-        text: 'Gain.Energy is proud to announce the partnership with the University of Sydney to address the perennial  problem of Oil and Gas data quality, though Agentic AI. Teaming up with a group of 7 postgrad data scientists, we are embarking on the development of a 𝗗𝗮𝘁𝗮 𝗤𝗔/𝗤𝗖 𝗔𝗜 𝗔𝗴𝗲𝗻𝘁, paving the way for enhanced data reliability and integrity.'
-    },
-    {
-        imageSrc: news7,
-        date: '1 June 2025',
-        text: 'Gain.Energy proudly announced a partnership with the Heriot-Watt University to develop custom-made AI Agents to address specific use cases. The first one to be built is a Drilling Rig Selection AI Agent that will optimize the process of identifying the best-suited rig and significantly reduce cost.'
-    },
-    {
-        imageSrc: news6,
-        date: '23 April 2025',
-        text: 'Our CEO Dr. Vlad K Payrazyan presented Upstrima AI Marketplace at GOTECH SPE Conference in Dubai. The presentation covered the unique in the industry concept of AI Marketplace and also several AI Agents in action, such as Best Composite Well Agent, End-of-Well Report Agent, Drilling Complications Agent and Rig Move Agent. The presentation was very well received.'
-    },
-    {
-        imageSrc: news5,
-        date: '11 Apr 2025',
-        text: 'Gain.Energy announced Upstrima AI Agents Marketplace.<br /> Upstrima Marketplace is aimed to host hundreds of specialized task-specific, goal-oriented and data-driven AI Agents, covering everything from routine calculations and data QA/QC, to offset well analytics, workflow automation, and complex engineering modeling. Each one is laser-focused on streamlining processes, boosting accuracy, and helping engineers make better decisions faster.'
-    },
-    {
-        imageSrc: news4,
-        date: '9 Jan 2025',
-        text: 'Gain.Energy and Merlin ERD Announce Strategic Partnership to Revolutionize Drilling Engineering with AI Gain.Energy, a pioneering provider of AI solutions for the Oil and Gas industry, forges a strategic partnership with Merlin ERD, a leading drilling engineering consulting firm renowned for its expertise in complex drilling projects. This collaboration brings together Gain.Energy’s cutting-edge AI technology and Merlin ERD’s deep domain knowledge to optimize drilling operations, reduce costs, and enhance safety for energy companies worldwide.'
-    },
-    {
-        imageSrc: news3,
-        date: '16 Dec 2024',
-        text: 'Our abstract for SPE GOTECH 2025 Conference has been accepted. We look forward to presenting UPSTRIMA AI at this great event in Dubai this April.'
-    },
-    {
-        imageSrc: news2,
-        date: '28 Nov 2024',
-        text: 'Gain.Energy enters into a partnership agreement with EMDAD to bring advanced AI technologies to UAE’s energy sector. This collaboration will empower EMDAD and it’s esteemed clients to leverage Gain.Energy’s state-of-the-art AI solutions to optimize operations, enhance efficiency, and drive innovation across the entire value chain.'
-    },
-    {
-        imageSrc: news1,
-        date: '5 Nov 2024',
-        text: 'Our CEO Vlad K. Payrazyan presented Upstrima at ADIPEC 2024 Conference and Exhibition in Abu Dhabi, UAE.'
-    }
-]
+  {
+    imageSrc: news18,
+    date: "5-6 August 2026",
+    text: `<span style="font-weight: 600;">Gain.Energy</span> will participate in the <span style="font-weight: 600;">SPE/IADC Asia Pacific Drilling Technology Conference & Exhibition 2026</span> in Bali, Indonesia. Throughout the two-day event, we will connect with industry professionals while highlighting the role of specialized AI agents in modern engineering workflows. On <span style="font-weight: 600;">6 August at 11:00 AM</span>, our CEO & Founder, <span style="font-weight: 600;">Dr. Vlad Karen Payrazyan</span>, will present "<span style="font-weight: 600;">Applying Specialized AI Agents for Plug and Abandonment Operation Analysis and Regulatory Compliance: A Fundamental Shift Towards Automating Workflows and Augmenting Engineers' Capabilities.</span>"`,
+  },
+  {
+    imageSrc: news17,
+    date: "14 July 2026",
+    text: `We're launching Upstrima - the first marketplace of specialized AI agents, tools and workflows built exclusively for oil & gas.    Join us to see a live demo of everything Upstrima has to offer from Intelligent data processing to Human-AI Validation Loop and much more. <br/><br/><p style="word-break: break-all;">Please register here: <a target="_blank" rel="noopener noreferrer" href="https://form.jotform.com/261755735879072" style="color: white;">https://form.jotform.com/261755735879072</a><p>`,
+  },
+  {
+    imageSrc: news16,
+    date: "22-24 June 2026",
+    text: `The <span style="font-weight: 600;">Gain.Energy</span> team exhibited at URTeC 2026, where we connected with geoscientists, engineers, operators, and industry leaders while showcasing AI innovations for the future of unconventional resource development. We showcased <span style="font-weight: 600;">Upstrima</span> during the <span style="font-weight: 600;">New Technology Showcase</span> program, where our CEO, Dr. Vlad Karén Payrazyan, delivered the live pre-launch of <span style="font-weight: 600;">UPSTRIMA - </span><span style="font-weight: 600;">the Marketplace of Specialized Oil and Gas AI Agents and Workflows</span>.`,
+  },
+  {
+    imageSrc: news15,
+    date: "17 Mar 2026",
+    text: `The <span style="font-weight: 600;">Gain.Energy</span> team exhibited at <span style="font-weight: 600;">IADC/SPE International Drilling Conference & Exhibition 2026</span>, connecting with industry leaders and engaging in technical discussions on the future of drilling operations. Our booth showcased how Data-Driven AI Agents automate workflows and enhance engineer capabilities in the energy sector. IADC/SPE International Drilling Conference 2026, with its dynamic exchanges and forward-looking sessions, was a major success for Gain.Energy.`,
+  },
+  {
+    imageSrc: news14,
+    date: "4 Feb 2026",
+    text: `Gain.Energy and Labrador invite you to an exclusive AI Workshop in Perth, Australia, on February 4th. This session will concentrate on the practical application of advanced Artificial Intelligence within the Oil and Gas sector, specifically addressing the accelerated adoption of AI across the industry. Our objective is to democratize AI by equipping engineers and domain experts with specialized, accessible tools to foster enhanced efficiency and innovation. <br/><br/>
+        <p style="word-break: break-all;">Please register here to confirm your attendance: <a target="_blank" rel="noopener noreferrer" href="https://form.jotform.com/253565778909275" style="color: white;">https://form.jotform.com/253565778909275</a><p>`,
+  },
+  {
+    imageSrc: news13,
+    date: "23 Dec 2025",
+    text: "Gain.Energy, in collaboration with ONYX iES, cordially invites you to an exclusive AI Workshop session in Kuala Lumpur. This workshop will provide an in-depth examination of the practical application of advanced Artificial Intelligence within the Oil and Gas sector, with a focus on accelerating AI adoption across the industry. Our core objective is to democratize AI for end-users by empowering engineers and domain experts with specialized and accessible AI tools, enabling greater efficiency and innovation.",
+  },
+  {
+    imageSrc: news12,
+    date: "5 November 2025",
+    text: "Gain.Energy invites you to a live Lunch & Learn on December 2, 2025, at The ION District, Houston, Texas. The session, “Specialized AI Agents for the Oil and Gas Industry: Practical Engineering, Use Cases, Demos, and Limitations,” will focus on the strategic application of Data-Driven AI Agents. This in-depth, hands-on event is for technical specialists and operational leadership, covering real-world engineering, live demos, and an objective analysis of current constraints.",
+  },
+  {
+    imageSrc: news11,
+    date: "3 November 2025",
+    text: "Dr. Vlad Karén Payrazyan, our CEO, delivered a keynote in the AI Zone at ADIPEC on November 3, showcasing live demonstrations of how Data-Driven AI Agents automate workflows and enhance engineer capabilities in the energy sector. ADIPEC 2025, with its interactive presentations and technical discussions, was a major success for Gain.Energy.",
+  },
+  {
+    imageSrc: news10,
+    date: "3 November 2025",
+    text: "From November 3–6, Gain.Energy exhibited at ADIPEC 2025 in Abu Dhabi, showcasing our cutting-edge Data-Driven AI Agents transforming oil & gas operations. CEO Dr. Vlad Karén Payrazyan spoke in the AI Zone on automation, augmented engineering, and performance-driven intelligence. Attendees saw live demonstrations of how AI achieves operational excellence and efficiency in upstream and downstream processes. We thank everyone who connected with us.",
+  },
+  {
+    imageSrc: news9,
+    date: "29 October 2025",
+    text: "Gain.Energy is joining forces with Bardasz to enable the seamless integration of Upstrima AI Solutions directly with real-time drilling & completions data through WITSML. This partnership allows our AI Agents and Workflows to process, interpret, and act on data as it is generated! Soon to come - Live AI Agents.",
+  },
+  {
+    imageSrc: news8,
+    date: "7 August 2025",
+    text: "Gain.Energy is proud to announce the partnership with the University of Sydney to address the perennial  problem of Oil and Gas data quality, though Agentic AI. Teaming up with a group of 7 postgrad data scientists, we are embarking on the development of a 𝗗𝗮𝘁𝗮 𝗤𝗔/𝗤𝗖 𝗔𝗜 𝗔𝗴𝗲𝗻𝘁, paving the way for enhanced data reliability and integrity.",
+  },
+  {
+    imageSrc: news7,
+    date: "1 June 2025",
+    text: "Gain.Energy proudly announced a partnership with the Heriot-Watt University to develop custom-made AI Agents to address specific use cases. The first one to be built is a Drilling Rig Selection AI Agent that will optimize the process of identifying the best-suited rig and significantly reduce cost.",
+  },
+  {
+    imageSrc: news6,
+    date: "23 April 2025",
+    text: "Our CEO Dr. Vlad K Payrazyan presented Upstrima AI Marketplace at GOTECH SPE Conference in Dubai. The presentation covered the unique in the industry concept of AI Marketplace and also several AI Agents in action, such as Best Composite Well Agent, End-of-Well Report Agent, Drilling Complications Agent and Rig Move Agent. The presentation was very well received.",
+  },
+  {
+    imageSrc: news5,
+    date: "11 Apr 2025",
+    text: "Gain.Energy announced Upstrima AI Agents Marketplace.<br /> Upstrima Marketplace is aimed to host hundreds of specialized task-specific, goal-oriented and data-driven AI Agents, covering everything from routine calculations and data QA/QC, to offset well analytics, workflow automation, and complex engineering modeling. Each one is laser-focused on streamlining processes, boosting accuracy, and helping engineers make better decisions faster.",
+  },
+  {
+    imageSrc: news4,
+    date: "9 Jan 2025",
+    text: "Gain.Energy and Merlin ERD Announce Strategic Partnership to Revolutionize Drilling Engineering with AI Gain.Energy, a pioneering provider of AI solutions for the Oil and Gas industry, forges a strategic partnership with Merlin ERD, a leading drilling engineering consulting firm renowned for its expertise in complex drilling projects. This collaboration brings together Gain.Energy’s cutting-edge AI technology and Merlin ERD’s deep domain knowledge to optimize drilling operations, reduce costs, and enhance safety for energy companies worldwide.",
+  },
+  {
+    imageSrc: news3,
+    date: "16 Dec 2024",
+    text: "Our abstract for SPE GOTECH 2025 Conference has been accepted. We look forward to presenting UPSTRIMA AI at this great event in Dubai this April.",
+  },
+  {
+    imageSrc: news2,
+    date: "28 Nov 2024",
+    text: "Gain.Energy enters into a partnership agreement with EMDAD to bring advanced AI technologies to UAE’s energy sector. This collaboration will empower EMDAD and it’s esteemed clients to leverage Gain.Energy’s state-of-the-art AI solutions to optimize operations, enhance efficiency, and drive innovation across the entire value chain.",
+  },
+  {
+    imageSrc: news1,
+    date: "5 Nov 2024",
+    text: "Our CEO Vlad K. Payrazyan presented Upstrima at ADIPEC 2024 Conference and Exhibition in Abu Dhabi, UAE.",
+  },
+];
 
 export default news;
